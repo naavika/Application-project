6ab39f86ebc44d349c16bcb3ef1238836d5d50ad
naavika <niksshahiruchi@gmail.com>
2024-08-10 19:44:52 +0530

    rendering user list

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,11 +3,11 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import "./AddUser.css";
 
-const AddUser = () => {
+const AddUser = (props) => {
 
   const [enteredUsername, setEnteredUsername] = useState("");
    const [enteredAge, setEnteredAge] = useState("");
-   const [error, setError] = useState('');
+   
 
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
@@ -20,18 +20,15 @@ const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      setError('Both fields are required.');
       return;
   }
 
   if (+enteredAge <= 0) {
-      setError('Age must be a positive number.');
       return;
   }
 
-  setError('');
-
-    console.log(enteredUsername, enteredAge);
+  const newUser = { name: enteredUsername, age: enteredAge };
+  props.onAddUser(newUser);
 
 
     setEnteredUsername('');
